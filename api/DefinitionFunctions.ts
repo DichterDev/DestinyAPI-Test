b71f7370bb807240364c29d64ac7e8c3e36dc129
DichterDev <david.dr.ruescher@outlook.com>
2023-05-25 13:44:45 +0200

    feat: refactored DefinitionFunction for performance

--- api/DefinitionFunctions.ts
+++ api/DefinitionFunctions.ts
@@ -1,7 +1,9 @@
 import downloadSQLITEDatabase from '../definitions/getDefinitions'
-import { Database } from 'sqlite3'
+import Database from 'better-sqlite3';
 
-
+interface queryResult {
+    value: string
+}
 
 interface Weapon {
     hash: string,
@@ -24,10 +26,12 @@ interface Perk {
 }
 
 let db = new Database('./definitions/DestinyDefinitions.sqlite3');
+db.pragma('journal_mode = WAL');
+
 
 let base_url = "https://bungie.net";
 
-export async function getWeapon(hash: string) {
+export function getWeapon(hash: string): Weapon {
     let query = `
     SELECT json_object(
     'hash', json_extract(d.json, '$.hash'),
@@ -38,15 +42,12 @@ export async function getWeapon(hash: string) {
     FROM DestinyInventoryItemDefinition d,
     json_each(json_extract(d.json, '$.sockets.socketEntries'))
     WHERE json_extract(d.json, '$.hash') = ${hash}`;
-    return new Promise<Weapon>(function(resolve) {
-        db.get(
-            query,
-            (_, res:any) => resolve(JSON.parse(res['value']))
-        );
-    })
+
+    let result = db.prepare(query).get() as queryResult;
+    return JSON.parse(result.value)
 }
 
-export async function getAllWeapons() {
+export function getAllWeapons(): Weapon[] {
     let query = `
     SELECT json_object(
     'hash',json_extract(d.json, '$.hash'),
@@ -58,30 +59,22 @@ export async function getAllWeapons() {
     json_each(json_extract(d.json, '$.sockets.socketEntries'))
     WHERE json_extract(d.json, '$.itemType') = 3
     GROUP BY d.id;`;
-    
-    return new Promise<Weapon[]>(function(resolve) {
-        db.all(
-            query,
-            (_,res:any[]) => resolve(res.map(row => {
-                let _row: Weapon = JSON.parse(row['value']);
-                return _row;
-            }))
-        );
+
+    let result = db.prepare(query).all() as queryResult[];
+    let weapons = result.map(function(value) {
+        return JSON.parse(value.value)
     })
+    return weapons
 }
 
-export async function getClassName(hash: string) {
+export function getClassName(hash: string) {
     let query = `
     SELECT json_extract(
     json, '$.displayProperties.name') as value 
     FROM DestinyClassDefinition 
     WHERE json_extract(json, '$.hash') = ${hash}`;
-    return new Promise(function(resolve) {
-        db.get(
-            query,
-            (_,res:any) => resolve(res.value)
-        );
-    });
+
+
 }
 
 export function getPerk(hash: string) {
@@ -92,47 +85,17 @@ export function getPerk(hash: string) {
     'description', json_extract(json, '$.displayProperties.description'), 
     'icon', json_extract(json,'$.displayProperties.icon')) as value 
     FROM DestinyInventoryItemDefinition 
-    WHERE json_extract(json, '$.hash') = ${hash}`
-    return new Promise<Perk>(function(resolve){
-        db.get(
-            query,
-            function(err, res: any) {
-                if (err === null) {
-                    resolve(JSON.parse(res.value));
-                }
-            }
-        );
-    })
+    WHERE json_extract(json, '$.hash') = ${hash}`;
+
+    let result = db.prepare(query).get() as queryResult;
+    return JSON.parse(result.value)
 }
 
-export async function getPerksOfWeapon(hash: string) {
-    let weapon: Weapon = await getWeapon(hash);
-    return weapon.perkHashes.filter(perkHash => perkHash !== 0).map(async function(perkHash) {
-        return await getPerk(perkHash.toString());
-    })
+export function getPerksOfWeapon(hash: string): Perk[] {
+    let weapon = getWeapon(hash);
+    return weapon.perkHashes.filter(hash => hash != 0).map(function(hash) {
+        return getPerk(hash.toString())
+    });
 }
 
-async function resolveSomething() {
-    // let weapon:Weapon = await getWeapon('3371017761');
-    // console.log(weapon);
-
-    // let weapons:Weapon[] = await getAllWeapons();
-    // console.log(weapons);
-
-    // let _class = await getClassName('2271682572');
-    // console.log(_class);
-
-    // let perk = await getPerk('1431678320');
-    // console.log(perk);
-
-/*     [3787406018,3492498209,1431678320,1105347996,3917450714,0,0,0,0,2302094943,1498917124,0,0,0,0,737953349].forEach(async function(perkHash) {
-        let perk = await getPerk(perkHash.toString());
-        console.log(perkHash);
-        console.log(perk);
-    }); */
-
-    let perks = await getPerksOfWeapon('3371017761');
-    console.log(perks);
-}
-
-resolveSomething();
+console.log(getPerksOfWeapon('3371017761'));
